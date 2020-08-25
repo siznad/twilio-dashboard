9887dacc476ef4a97398aee4baf19d8d55f7db8f
John McGaughey <siznad@gmail.com>
2020-08-25 14:49:33 -0500

    added new route

--- taskrouter_dashboard.py
+++ taskrouter_dashboard.py
@@ -232,8 +232,7 @@ def getcallstats():
     request_dict = {}
     request_dict = request.form.to_dict()
 
-    for k,y in request_dict.items():
-        print(k, y)
+    print(request_dict)
 
     url = 'https://insights.twilio.com/v1/Voice/' + request_dict['callSid'] + '/Metrics'
     response = requests.request("GET", url, auth=HTTPBasicAuth(twilio_account_sid, twilio_auth_token))
