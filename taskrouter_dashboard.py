9edd6133cfa9e83f23e1012fa8daf12beb685955
John McGaughey <siznad@gmail.com>
2020-08-26 15:10:58 -0500

    removed getcallstats

--- taskrouter_dashboard.py
+++ taskrouter_dashboard.py
@@ -251,8 +251,8 @@ def alarms():
     print(request_dict['Timestamp'])
     print(payload['webhook']['request']['method'])
     print(payload['webhook']['request']['parameters'])
-    print(payload['webhook']['response'])
-    print(payload['webhook']['body'])
+    print(payload['webhook']['response']['status_code'])
+    print(payload['webhook']['response']['body'])
 
     return 'OK'
 
