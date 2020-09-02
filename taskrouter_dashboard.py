f4e3c46526f7635aa06310e1d48f736b5e7acb8d
John McGaughey <siznad@gmail.com>
2020-09-02 15:40:46 -0500

    tz

--- taskrouter_dashboard.py
+++ taskrouter_dashboard.py
@@ -49,7 +49,7 @@ def sync_taskrouter_statistics():
         task_status = k + 'Tasks'
         stats[task_status] = statistics.realtime['tasks_by_status'][k]
 
-    stats['completedTasks'] = cumulativeStats['tasks_completed']
+    stats['completedTasks'] = cumulativeStats.tasks_completed
     stats['canceledTasks'] = statistics.cumulative['tasks_canceled']
 
     print(stats)
